2bcdc949150079d741fff13512030db3f9b10de9
Dakose <bohdanholubn@gmail.com>
2024-02-06 17:34:12 +0200

    services
    create services module

--- petwebapp/petwebapp/urls.py
+++ petwebapp/petwebapp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import services
 
 urlpatterns = [
+    path('', services, name='home'),
     path('admin/', admin.site.urls),
 ]
