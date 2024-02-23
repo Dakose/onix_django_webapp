da3a938d6f80f81e0efd21bcdba32b17969e602c
Dakose <bohdanholubn@gmail.com>
2024-02-23 14:19:53 +0200

    about, detail views post

--- petwebapp/authentication/urls.py
+++ petwebapp/authentication/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from services.views import HomeView, ArticleDetailView
+from about.views import about
 
 urlpatterns = [
     path('authentication_page', views.authentication_page, name="authentication_page"),
     path('home', HomeView.as_view(), name="home"),
+    path('about', about, name="about"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
     path('signout', views.signout, name="signout"),
