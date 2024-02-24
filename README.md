dbff2ac1093e4b6fea150d3df35a44fa08d51430
Dakose <bohdanholubn@gmail.com>
2024-02-24 11:09:48 +0200

    navbar bootstrap

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 #add authentication page , with signin/sighup/signout methods
 #add services page, with posts
+#create navbar
+#add basic bootstrap
--- petwebapp/services/views.py
+++ petwebapp/services/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import UserService
 
-# def home(request):
-#     return render(request, 'home.html', {})
-
 class HomeView(ListView):
     model = UserService
     template_name = 'home.html'
