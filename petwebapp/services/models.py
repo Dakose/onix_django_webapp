47e234ee233bdef7e653b507e1188ef07768159a
Dakose <bohdanholubn@gmail.com>
2024-03-04 19:08:50 +0200

    Update models.py

--- petwebapp/services/models.py
+++ petwebapp/services/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Category:")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Category:"
+        verbose_name_plural = "Category:"
 class UserService(models.Model):
     dt = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,6 +20,7 @@ class UserService(models.Model):
     text = models.TextField()
     image = models.ImageField(upload_to="serviceimg/", null=True, blank=True, default=None, verbose_name="Image:")
     title_tag = models.CharField(max_length=255, verbose_name="Title tag:", default="Other")
+    # category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True, verbose_name="Status:", default="Other")
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
